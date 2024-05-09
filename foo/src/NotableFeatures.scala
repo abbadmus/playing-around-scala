d2171756a770ebb8307312236f019ab34d9091e8
abbadmus <abdulbadmus75@gmail.com>
2024-05-09 22:55:31 +0100

    adding Json traits

--- foo/src/NotableFeatures.scala
+++ foo/src/NotableFeatures.scala
@@ -1,3 +1,26 @@
+package foo
+
 object NotableFeatures {
+  def main(args: Array[String]): Unit = {
+
+    def getDayMonthYear(s: String) = s match {
+      case s"$day-$month-$year" => println(s"found day: $day, month: $month, year: $year")
+      case _ => println("not a date")
+    }
+
+    getDayMonthYear("10-12-1994")
+    getDayMonthYear("12-1994")
+
+
+  }
+
+  sealed trait Json
+  case class Null() extends Json
+  case class Bool(value: Boolean) extends Json
+  case class Str(value: String) extends Json
+  case class Num(value: Double) extends Json
+  case class Arr(value: Seq[Json]) extends Json
+  case class Dict(value: Map[String, Json]) extends Json
+
 
 }
