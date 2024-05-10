6341fc738c05e3616e2b7bbc8e2541c17f5cf7c9
abbadmus <abdulbadmus75@gmail.com>
2024-05-10 15:57:10 +0100

    adding implicits assignment

--- foo/src/UnderstandingAssignment.scala
+++ foo/src/UnderstandingAssignment.scala
@@ -1,6 +1,34 @@
 object UnderstandingAssignment {
   def main(args: Array[String]): Unit = {
 
+
   }
+
+  trait StringParser[T]{
+    def parse(s: String): T
+  }
+
+  object StringParser{
+    implicit object IntParser extends StringParser[Int] {
+      override def parse(s: String): Int = s.toInt
+    }
+    implicit object BoolParser extends StringParser[Boolean] {
+      override def parse(s: String): Boolean = s.toBoolean
+    }
+    implicit object DoubleParser extends StringParser[Double] {
+      override def parse(s: String): Double = s.toDouble
+    }
+  }
+
+  def fromStringParser[T: StringParser](s: String) = {
+    val parser = implicitly[StringParser[T]]
+    parser.parse(s)
+
+  }
+
+  import StringParser.IntParser
+
+  println(fromStringParser("45"))
+
 
 }
